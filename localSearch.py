d6e44f585019ba140cf71142163ecceee82ee618
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-23 15:54:14 +0300

    add pertubation

--- localSearch.py
+++ localSearch.py
@@ -1,6 +1,6 @@
 import math
 from random import random,sample,randint
-
+from greedy import algorithm
 def read_data(n):
     return [input() for i in range(n)]
 
@@ -9,7 +9,7 @@ def form_lists(data):
     for i in data:
         i = list(map(float,i.strip().split()))
         id_list.append(int(i[0]))
-        cords_list.append(i[1:])
+        cords_list.append((i[1:]))
     return id_list,cords_list
 
 def two_point_dist(point_one,point_two):
@@ -22,21 +22,23 @@ def calc_result_distance(route,cords_list):
         res_distance+=two_point_dist(cords_list[route[i]],cords_list[route[i-1]])
     return res_distance
 
-def swap_two_opt(route,i,k):
-    new_route = route[:i]
-    new_route += reversed(route[i:k])
-    new_route += route[k:]
-    return new_route
-       
+
 def two_opt(route,distance):
     for i in range(len(route)):
         for k in range(i,len(route)):
-            improved_route = swap_two_opt(route[:],i,k)
+            improved_route = route[:i] + route[i:k][::-1] + route[k:]
             new_distance = calc_result_distance(improved_route,cords_list)
             if new_distance<distance :
                 distance=new_distance
                 route=improved_route
     return route
+
+def do_pertubation(route):
+    new_route = route[:]
+    i=randint(0,len(route)-1)//4
+    j=i+randint(0,len(route)-1)//4
+    k=j+randint(0,len(route)-1)//4
+    return route[:i]+route[k:] + route[j:k]+route[i:j]
 
 def local_search(start_route, cords_list):
     res_distance = calc_result_distance(start_route,cords_list)
@@ -44,10 +46,23 @@ def local_search(start_route, cords_list):
 
 def ILS(id_list,cords_list):
     start_route = sample([x for x in range(len(id_list))],len(id_list))
+    # print(start_route,dist)
+    
     route = local_search(start_route,cords_list)
     min_distance = calc_result_distance(route,cords_list)
     print("Distance after 2-Opt Approach:",min_distance)
+    print(route,len(route))
+    for i in range(len(route)):
+        new_route = do_pertubation(route)
+        new_route=local_search(new_route,cords_list)    
+        new_distance=calc_result_distance(new_route,cords_list)
+        if(new_distance<min_distance):
+            min_distance=new_distance
+            route=new_route    
+    print("Distance after Iterated 2-Opt Approach:",min_distance)
     print(route)
+    return route
+        
 
 
 id_list,cords_list = form_lists(read_data(52))
