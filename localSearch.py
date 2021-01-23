59a09000d22f4bb172ffeaa23d1c5640a372fa85
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-23 13:01:15 +0300

    maybe random is bad practice for start route???

--- localSearch.py
+++ localSearch.py
@@ -7,7 +7,7 @@ def read_data(n):
 def form_lists(data):
     id_list,cords_list = [],[]
     for i in data:
-        i = list(map(int,i.strip().split()))
+        i = list(map(float,i.strip().split()))
         id_list.append(int(i[0]))
         cords_list.append(i[1:])
     return id_list,cords_list
@@ -19,26 +19,35 @@ def two_point_dist(point_one,point_two):
 def calc_result_distance(route,cords_list):
     res_distance=0
     for i in range(1,len(route)):
-        res_distance+=two_point_dist(cords_list[route[i]],coords_list[route[i-1]])
+        res_distance+=two_point_dist(cords_list[route[i]],cords_list[route[i-1]])
     return res_distance
-        ``
+        
 def two_opt(route,distance):
     for i in range(1, len(route)-2):
            for j in range(i+1, len(route)):
-                if j-i == 1: continue # changes nothing, skip then
+                if j-i == 1: continue 
                 improved_route = route[:]
                 improved_route[i:j] = route[j-1:i-1:-1]
                 new_distance=calc_result_distance(improved_route,cords_list)
-            if(new_distance<distance):
-                distance=new_distance
-                route=improved_route
+                if(new_distance<distance):
+                    distance=new_distance
+                    route=improved_route
     return route
 
-def local_search(id_list, cords_list):
-    start_route = sample(id_list,len(id_list))
+def local_search(start_route, cords_list):
     res_distance = calc_result_distance(start_route,cords_list)
-    return two_opt(route,res_distance)
+    return two_opt(start_route,res_distance)
 
-id_list,cords_list = form_lists(read_data(4))
+def ILS(id_list,cords_list):
+    
+    start_route = sample([x for x in range(len(id_list))],len(id_list))
+    route = local_search(start_route,cords_list)
+    min_distance = calc_result_distance(route,cords_list)
+    print("Distance after 2-Opt Approach:",min_distance)
+    print(route)
 
-# ILS(id_list,cords_list)
+
+
+id_list,cords_list = form_lists(read_data(52))
+
+ILS(id_list,cords_list)
